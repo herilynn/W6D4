4ac72b0ce30f27ebf6fac1732ee3f2b3878fcf2e
herilynn <4henrylin17@gmail.com>
2023-04-13 15:37:15 -0400

    Complete ArtworkShare connections

--- ArtShareAPI/app/models/artwork.rb
+++ ArtShareAPI/app/models/artwork.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :bigint           not null, primary key
+#  image_url  :string
+#  artist_id  :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  title      :string           not null
+#
 class Artwork < ApplicationRecord
     validates :title, :artist_id, :image_url, presence: true
     validates :image_url, uniqueness: true
@@ -6,9 +17,13 @@ class Artwork < ApplicationRecord
         foreign_key: :artist_id, 
         class_name: :User 
 
-    has_many :viewed_art,
-    foreign_key: :artist_id,
+    has_many :shares,
+    foreign_key: :artwork_id,
     class_name: :ArtworkShare,
     dependent: :destroy
+
+    has_many :shared_viewers,
+    through: :shares,
+    source: :viewer
 
 end
--- ArtShareAPI/app/models/user.rb
+++ ArtShareAPI/app/models/user.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
@@ -10,5 +19,9 @@ class User < ApplicationRecord
   foreign_key: :viewer_id,
   class_name: :ArtworkShare,
   dependent: :destroy
+
+  has_many :shared_artwork,
+  through: :viewed_artwork,
+  source: :artwork
 
 end
